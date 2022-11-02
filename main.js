45ebbe133f71a2ea5bbf08cbfefa82b428cb4e17
= <=>
2022-11-02 11:20:34 +0100

    adding context menu

--- main.js
+++ main.js
@@ -14,11 +14,10 @@ var selectY=0;
 var fps = 60;
 var usage = 0.1;
 var NodeSize=3;
-var conSize=2;
+var conSize=3;
 var zMin = 0.5;
 var zMax = 3;
 var sped = 5;
-var keys="wasd";
 var mOffsetX = 0;
 var mOffsetY = 0;
 var boundX = 500
@@ -29,9 +28,21 @@ var gridSize = 25;
 var fpsFont = "30px arial";
 var cursorColor = "red"
 var selectedOutlineThiccness = 2;
-var selectedOutlineColour = "blue";
-var clearColor = "darkgray"
-var gridColor = "gray"
+var selectedOutlineColour = "#85c9e9";
+var selectorColor = "#8888ff55";
+var clearColor = "darkgray";
+var gridColor = "gray";
+var gridWidth = 2;
+var lineScale = 0.1;
+var contextBackground = "black";
+var contextSeparator = "gray";
+var contextWidth = 300;
+var contextTextOffsetL=10;
+var toolHeight = 30;
+var contextFont = "30px arial";
+var contextFontColour = "gray";
+var toolIgnoreIndex=3;
+
 
 var tools=[
     "Nothing",
@@ -45,6 +56,10 @@ var tools=[
     "Edit"
 ];
 
+var keys=["w","a","s","d",
+    "f","e","q",
+    "ArrowUp","ArrowLeft","ArrowDown","ArrowRight"];
+
 var nodeInfo=[
     0,0,5,
     10,10,10,
@@ -54,16 +69,15 @@ var nodeInfo=[
 ];
 
 var connectionsInfo=[
-    1,2,
-    1,3,
-    1,5,
-    2,3,
-    3,4
+    1,2,3,
+    1,3,2,
+    1,4,1,
+    2,2,4,
+    3,3,2
 ];
 
 class node{
     constructor(x,y, thicc){
-        this.connections=[];
         this.x=x;
         this.y=y;
         this.thicc=thicc;
@@ -72,9 +86,10 @@ class node{
 }
 
 class connection{
-    constructor(n1,n2) {
+    constructor(n1,n2,t) {
         this.node1=n1;
         this.node2=n2;
+        this.thicc=t;
     }
 }
 
@@ -83,6 +98,17 @@ class camera{
         this.x=0;
         this.y=0;
         this.zoom=1;
+    }
+}
+
+function generateNodes(){
+    let o=nodes.length;
+    for(let i=0; i<nodeInfo.length/NodeSize;i++){
+        nodes[o+i]=new node(nodeInfo[i*NodeSize],nodeInfo[i*NodeSize+1],nodeInfo[i*NodeSize+2])
+    }
+    o=connections.length;
+    for(let i=0; i<connectionsInfo.length/conSize;i++){
+        connections[o+i]= new connection(nodes[connectionsInfo[i*conSize]],nodes[connectionsInfo[i*conSize+1]],connectionsInfo[i*conSize+2])
     }
 }
 
@@ -106,39 +132,58 @@ function keyd(event){
     if(keys.includes(event.key)){
         keyStatuses[keys.indexOf(event.key)+1]=1;
     }
+    if(event.key="f"&&selectedNodes.length<4){
+
+    }
 }
 
 function mousem(event){
     mouseX=event.clientX+mOffsetX;
     mouseY=event.clientY+mOffsetY;
+    if(event.buttons%8>=4){
+        cam.x+=event.movementX/cam.zoom;
+        cam.y+=event.movementY/cam.zoom;
+        camMovement();
+    }
 }
 
 function moused(event){
-    selectedNodes=[];
-    toolSelected=tools.indexOf("Select");
-    selectX=nodes[0].x;
-    selectY=nodes[0].y;
+    if(event.button==0){
+        if(!event.shiftKey){
+            selectedNodes=[];
+        }
+        toolSelected=tools.indexOf("Select");
+        selectX=nodes[0].x;
+        selectY=nodes[0].y;
+    }
+
+    if(event.button==2){
+        if(toolSelected==0){
+            toolSelected=tools.indexOf("Menu");
+            selectX=event.clientX;
+            selectY=event.clientY;
+        }else{
+            toolSelected=0;
+        }
+    }
 }
 
 function mouseu(event){
-    if(toolSelected==tools.indexOf("Select")){
-        for(let i=1; i<nodes.length;i++){
-            if(intersects(nodes[i],(nodes[0].x+selectX)*0.5,(nodes[0].y+selectY)*0.5,Math.abs(nodes[0].x-selectX),Math.abs(nodes[0].y-selectY))){
-                selectedNodes[selectedNodes.length]=nodes[i]
+    if(event.button==0){
+        if(toolSelected==tools.indexOf("Select")){
+            for(let i=1; i<nodes.length;i++){
+                if(intersects(nodes[i],(nodes[0].x+selectX)*0.5,(nodes[0].y+selectY)*0.5,Math.abs(nodes[0].x-selectX),Math.abs(nodes[0].y-selectY))){
+                    selectedNodes[selectedNodes.length]=nodes[i]
+                }
             }
         }
-    }
-    toolSelected=0;
-}
-
-function mouser(event){
-    if(toolSelected==0){
-        toolSelected=tools.indexOf("Menu");
-        selectX=event.clientX;
-        selectY=event.clientY;
-    }else{
         toolSelected=0;
     }
+    
+    if(event.button==2){
+        
+    }
+    
 }
 
 function intersects(circle, rectx, recty, rectw, recth)
@@ -164,16 +209,16 @@ function pointerUpdate(){
 }
 
 function camMovement(){
-    if(getKey("w")==1){
+    if(getKey("ArrowUp")==1){
         cam.y+=sped/cam.zoom;
     }
-    if(getKey("s")==1){
+    if(getKey("ArrowDown")==1){
         cam.y-=sped/cam.zoom;
     }
-    if(getKey("a")==1){
+    if(getKey("ArrowLeft")==1){
         cam.x+=sped/cam.zoom;
     }
-    if(getKey("d")==1){
+    if(getKey("ArrowRight")==1){
         cam.x-=sped/cam.zoom;
     }
     cam.x=Math.min(Math.max(cam.x,-boundX),boundX);
@@ -217,22 +262,47 @@ function render(){
         ctx.lineTo(can.width, y+can.height*0.5+cam.y*cam.zoom);
     }
     ctx.stroke();
-    ctx.fillStyle="blue";
+    ctx.fillStyle=selectedOutlineColour;
     for(let i=0; i<selectedNodes.length;i++){
         let n=selectedNodes[i];
         n=new node(n.x,n.y,n.thicc+selectedOutlineThiccness);
-        ctx.fillStyle=selectedOutlineColour;
         ctx.fill(circle(n, cam));
     }
+    ctx.strokeStyle="black"
+    for(let i=0;i<connections.length;i++){
+        let c=connections[i];
+        ctx.beginPath();
+        ctx.moveTo((cam.x+c.node1.x)*cam.zoom+can.width*0.5,(cam.y+c.node1.y)*cam.zoom+can.height*0.5);
+        ctx.lineTo((cam.x+c.node2.x)*cam.zoom+can.width*0.5,(cam.y+c.node2.y)*cam.zoom+can.height*0.5);
+        ctx.lineWidth=(c.node1.thicc+c.node2.thicc)*cam.zoom*lineScale;
+        ctx.stroke();
+    }
+    
     for(let i=1;i<nodes.length;i++){
         let n=nodes[i];
         ctx.fillStyle=n.color;
         ctx.fill(circle(n, cam));
     }
+    if(toolSelected==tools.indexOf("Select")){
+        ctx.fillStyle=selectorColor;
+        ctx.rect(Math.min((selectX+cam.x)*cam.zoom,mouseX)+can.width*0.5,Math.min((selectY+cam.y)*cam.zoom,mouseY)+can.height*0.5,Math.abs(mouseX-(selectX+cam.x)*cam.zoom),Math.abs(mouseY-(selectY+cam.y)*cam.zoom));
+        ctx.fill();
+    }
     ctx.font=fpsFont;
     ctx.fillStyle="darkblue";
     ctx.fillText("FPS: "+Math.round(fps),10,30)
     ctx.fillText("Usage%: "+Math.round(100*usage),10,60)
+    if(toolSelected==tools.indexOf("Menu")){
+        ctx.fillStyle=contextBackground;
+        ctx.fillRect((selectX+cam.x)*cam.zoom,(selectY-cam.y)*cam.zoom+can.width*0.5,contextWidth, toolHeight*tools.length);
+        ctx.stroke();
+        ctx.font=contextFont;
+        ctx.fillStyle=contextFontColour;
+        for(let i=0;i<tools.length-toolIgnoreIndex;i++){
+            ctx.fillText(tools[i+toolIgnoreIndex],(selectX-cam.x)*cam.zoom+can.width*0.5+contextTextOffsetL,(selectY+cam.y+i*toolHeight)*cam.zoom);
+        }
+    }
+    
     ctx.fillStyle=cursorColor;
     ctx.fill(circle(nodes[0], cam));
 }
@@ -250,17 +320,11 @@ function load(){
     window.addEventListener('mousemove', mousem);
     can.addEventListener('mousedown', moused);
     can.addEventListener('mouseup', mouseu);
-    can.addEventListener('contextmenu', mouser);
     can.addEventListener("wheel", function(e) {
         cam.zoom-=0.1*Math.sign(e.deltaY);
         cam.zoom=Math.min(zMax,Math.max(zMin, cam.zoom))
     });
-    for(let i=0; i<nodeInfo.length/NodeSize;i++){
-        nodes[i]=new node(nodeInfo[i*NodeSize],nodeInfo[i*NodeSize+1],nodeInfo[i*NodeSize+2])
-    }
-    for(let i=0; i<connectionsInfo.length/conSize;i++){
-        connections[i]= new connection(nodes[connectionsInfo[i*conSize]],nodes[connectionsInfo[i*conSize+1]])
-    }
+    generateNodes();
     for(let i=0; i<keys.length;i++){
         keyStatuses[i*2]=keys[i];
         keyStatuses[i*2+1]=0;
